0c421ac40825c0455c99fd6b1fc03da5bf4cfdf5
MathewJobey <mathewjobey17@gmail.com>
2025-07-25 20:28:13 +0530

    frame 400

---
+++ src/PongGame.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+
+public class PongGame{
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("🎾Ping Pong Chaos🎾");
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
